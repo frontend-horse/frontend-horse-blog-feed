c3017cea55c4796d72d00b84d7ce423455c0128a
Alex Trost <19617280+a-trost@users.noreply.github.com>
2024-01-03 10:25:23 -0500

    Switch to server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [sitemap()],
+  output: "server",
+  adapter: netlify(),
 });
